4b9fd7773b738a4aec5cc6131760c3b8102b8d8d
jtmst <josh.troy.mills@gmail.com>
2020-11-12 14:28:32 -0500

    set up rating component

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,15 @@ import React from "react";
 import { hot } from 'react-hot-loader/root';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Review from './ratings-reviews/Review'
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
-      <>
-        <h1>
-          Hello
-        </h1>
-      </>
+      <div>
+        <Review />
+      </div>
     );
   }
 }
