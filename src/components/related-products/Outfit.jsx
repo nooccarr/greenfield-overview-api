fb9bc341918343ca84bee718bf537ab52338237a
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2020-11-13 11:39:34 -0500

    Completed Skeleton for single prod in outfit

--- src/components/related-products/Outfit.jsx
+++ src/components/related-products/Outfit.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import OutfitProd from './Outfitprod'
 
 const Outfit = (props) => (
     <div>
         <h3>YOUR OUTFIT</h3>
+        <OutfitProd/>
     </div>
 )
 
---
+++ src/components/related-products/Outfitprod.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const OutfitProd = (props) => (
+    <div>
+        <span>this will be an item in the related products</span>
+    </div>
+)
+
+
+export default OutfitProd;
