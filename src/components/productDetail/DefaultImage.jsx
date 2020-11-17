bea53278511046e72a50cdc76747f77c74307c4a
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-11-16 23:24:17 -0500

    developing slider for the default images

--- src/components/productDetail/DefaultImage.jsx
+++ src/components/productDetail/DefaultImage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel, CarouselItem } from 'react-bootstrap';
 
 class DefaultImage extends React.Component {
   constructor(props) {
@@ -14,14 +15,32 @@ class DefaultImage extends React.Component {
 
   render() {
     return (
-      <div>
-        <img src={this.props.currentStyle.photos[0].url}></img>
-        <div>
-          {
-            // thumbnails
-            // each thumbnail should be able to toggle and handle click
-          }
-        </div>
+      <div className="default-image-container">
+        <Carousel>
+          {this.props.currentStyle.photos.map((photo, index) => {
+            return (
+              <Carousel.Item>
+                <img
+                  src={photo.url}
+                  key={index}
+                  style={{ height: `100%`, width: `100%` }}
+                />
+              </Carousel.Item>
+            );
+          })}
+          {/* <ol className="carousel-indicators">
+            {this.props.currentStyle.photos.map((photo, index) => {
+              return (
+                <li key={index} onClick={() => this.goToIndex(index)}>
+                  <img
+                    src={photo.thumbnail_url}
+                    style={{ height: `100%`, width: `100%` }}
+                  />
+                </li>
+              );
+            })}
+          </ol> */}
+        </Carousel>
       </div>
     );
   }
