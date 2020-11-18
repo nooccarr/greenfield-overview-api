208134736f31a204680680cd3129a2d5fa861e70
jtmst <josh.troy.mills@gmail.com>
2020-11-18 12:17:01 -0500

    basic review templating done

--- src/components/ratings-reviews/Review.jsx
+++ src/components/ratings-reviews/Review.jsx
@@ -7,12 +7,19 @@ function Review(props) {
     <div className="single-review">
       <Rating
         // value={props.allReviews.rating}
-        value={2}
+        value={props.indReview.rating}
         precision={0.1}
         readOnly
         size="small"
         id="review-stars"
       />
+      <p className="individual-review-user">{props.indReview.reviewer_name}</p>
+      <h3 className="individual-review-header">{props.indReview.summary}</h3>
+      <p className="individual-review-body">{props.indReview.body}</p>
+      <div className="individual-review-photos">
+        {/* some kind map magic here to render out review photos */}
+        <hr></hr>
+      </div>
     </div>
   );
 }
--- src/components/ratings-reviews/Reviews.jsx
+++ src/components/ratings-reviews/Reviews.jsx
@@ -2,14 +2,42 @@ import React, { useState, useEffect } from 'react';
 import Review from './Review.jsx';
 
 function Reviews(props) {
-  return (
-    <div className="reviews-container">
-      <h4>{props.reviewCount} reviews, sorted by</h4>
-      <div className="main-review-block">
-        <Review allRatings={props.allRatings} allReviews={props.allReviews} />
+  const reviewsFromProps = props.allReviews;
+
+  const renderReviews = (allReviews) => {
+    return allReviews.map((review) => {
+      console.log(review);
+      return (
+        <div className="reviews-container">
+          <div className="main-review-block">
+            <Review
+              allRatings={props.allRatings}
+              allReviews={props.allReviews}
+              indReview={review}
+            />
+          </div>
+        </div>
+      );
+    });
+  };
+
+  if (!props.allReviews) {
+    return (
+      <div className="reviews-container">
+        <h4 className="review-header">0 reviews for this item</h4>
       </div>
-    </div>
-  );
+    );
+  } else if (reviewsFromProps.length <= 2) {
+    return (
+      <div>
+        <h4 className="review-header">
+          {props.reviewCount} reviews, sorted by
+        </h4>
+        <div>{renderReviews(reviewsFromProps)}</div>
+      </div>
+    );
+  }
+  return <div></div>;
 }
 
 export default Reviews;
