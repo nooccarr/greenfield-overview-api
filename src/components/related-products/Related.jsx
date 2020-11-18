7e98859a8db0315e775d4034c95f3ae5bef8b5c8
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2020-11-18 16:33:56 -0500

    Arrows now disapear appropriately

--- src/components/related-products/Related.jsx
+++ src/components/related-products/Related.jsx
@@ -5,6 +5,7 @@ class Related extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            ref: 0
         }
         
     }
@@ -14,25 +15,49 @@ class Related extends React.Component {
     rightClick() {
         const slide = this.myRef.current
         slide.scrollLeft += 330
+        this.setState({
+            ref: this.state.ref+=330
+        })
+        // if(slide.scrollLeft >= (slide.scrollWidth - slide.offsetWidth)) {
+        //     slide.scrollLeft = 0
+        // }
     }
 
     leftClick() {
         const slide = this.myRef.current
         slide.scrollLeft -= 330
+        this.setState({
+            ref: this.state.ref-=330
+        })
+        // if(slide.scrollLeft <= 0) {
+        //     slide.scrollLeft = slide.offsetWidth
+        // }
     }
 
 
     leftButton() {
+        if(this.state.ref <= 0) {
+            return (
+                <button className='carouselHide' id='leftBut' onClick={() => {this.leftClick()}}></button>
+            )
+        } else {
             return (
                 <button className='carouselArrow carouselLeft' id='leftBut' onClick={() => {this.leftClick()}}></button>
             )
-        
+        }
     }
 
     rightButton() {
+        var checker = (this.props.related.length * 141.3)
+        if(this.state.ref >= checker) {
+            return (
+                <button className='carouselHide' id='rightBut' onClick={() => {this.rightClick()}}></button>
+            )
+        } else {
             return (
                 <button className='carouselArrow carouselRight' id='rightBut' onClick={() => {this.rightClick()}}></button>
             )
+        }
     }
 
 
