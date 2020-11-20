17e1beb26824e467aa0737e1ca73d33697a17d9f
jtmst <josh.troy.mills@gmail.com>
2020-11-19 20:33:41 -0500

    added sorting to review list

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-
         <header>
           <h1 class="main-logo">DANGO</h1>
           <div class="search-wrap">
--- src/components/ratings-reviews/Rating-Helpers.js
+++ src/components/ratings-reviews/Rating-Helpers.js
@@ -1,17 +1,36 @@
 
 function findRatingAverage(ratings) {
-    let ratingProduct = Object.keys(ratings).map((key, value) => {
-        return Object.values(ratings)[value] * key;
-    });
-    let averageRating = ratingProduct.reduce((a, b) => {
-        return (
-            (a + b) /
-            Object.values(ratings).reduce((a, b) => {
-                return a + b;
-            })
-        );
-    });
-    return Math.round(averageRating * 10) / 10
+    // let ratingProduct = Object.keys(ratings).map((key, value) => {
+    //     return Object.values(ratings)[value] * key;
+    // });
+    // let averageRating = ratingProduct.reduce((a, b) => {
+    //     return (
+    //         (a + b) /
+    //         Object.values(ratings).reduce((a, b) => {
+    //             return a + b;
+    //         })
+    //     );
+    // });
+    // return Math.round(averageRating * 10) / 10
+    if (!ratings[1]) {
+        ratings[1] = 0
+    }
+    if (!ratings[2]) {
+        ratings[2] = 0
+    }
+    if (!ratings[3]) {
+        ratings[3] = 0
+    }
+    if (!ratings[4]) {
+        ratings[4] = 0
+    }
+    if (!ratings[5]) {
+        ratings[5] = 0
+    }
+    let sumTotal = ratings[1] * 1 + ratings[2] * 2 + ratings[3] * 3 + ratings[4] * 4 + ratings[5] * 5
+
+    let valSum = ratings[1] + ratings[2] + ratings[3] + ratings[4] + ratings[5]
+    return (Math.round((sumTotal / valSum) * 10) / 10)
 }
 
 function findRecommendPercent(recommended) {
@@ -22,13 +41,11 @@ function findRecommendPercent(recommended) {
 function getRatingPercentages(ratings) {
     let largest = 0;
     let largestKey
-    // let valueSum = Object.values(ratings).reduce((a, b) => a + b)
+    let valueSum = Object.values(ratings).reduce((a, b) => a + b)
     // console.log(ratings)
     if (!ratings) {
         return { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
     }
-
-
 
     for (let val in ratings) {
         if (ratings[val] > largest) {
@@ -38,11 +55,11 @@ function getRatingPercentages(ratings) {
     }
 
     let ratingPercentages = {
-        "1": (ratings[1] / ratings[largestKey]) * 100,
-        "2": (ratings[2] / ratings[largestKey]) * 100,
-        "3": (ratings[3] / ratings[largestKey]) * 100,
-        "4": (ratings[4] / ratings[largestKey]) * 100,
-        "5": (ratings[5] / ratings[largestKey]) * 100
+        "1": (ratings[1] / valueSum) * 100,
+        "2": (ratings[2] / valueSum) * 100,
+        "3": (ratings[3] / valueSum) * 100,
+        "4": (ratings[4] / valueSum) * 100,
+        "5": (ratings[5] / valueSum) * 100
     }
 
     for (let val in ratingPercentages) {
