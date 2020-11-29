3923290ec5b46c6b9772f7b8f09619191734cf21
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-11-29 14:15:16 -0500

    removed dummy datas

--- src/components/productDetail/Default-features.jsx
+++ src/components/productDetail/Default-features.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 
 const Features = (props) => {
   // API data acting weird...
+
   console.log('this is features: ', props.currentProduct.features);
-  return (
-    <div className="default-features-container">
-      <ul className="default-features-container-list">
-        {props.currentProduct.features.map((item, index) => {
-          return (
-            <li key={index}>
-              {item.feature}: {item.value}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+  if (props.currentProduct.features === undefined) {
+    return (
+      <div>
+        <span>please wait for related product to load</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="default-features-container">
+        <ul className="default-features-container-list">
+          {props.currentProduct.features.map((item, index) => {
+            return (
+              <li key={index}>
+                {item.feature}: {item.value}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default Features;
