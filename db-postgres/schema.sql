8da98bd3a812a00c6a0c3785f50c67030b64fabc
Soohyun Lim <d2axasap@gmail.com>
2020-12-04 00:13:16 -0500

    Make transformation functions to work with actual data. Modified schema to create tables and copy transformed data into the database. Created csv splitter function to split photos.csv into two for faster transforming time.

--- db-postgres/schema.sql
+++ db-postgres/schema.sql
@@ -10,54 +10,55 @@ DROP TABLE IF EXISTS styles CASCADE;
 DROP TABLE IF EXISTS photos CASCADE;
 DROP TABLE IF EXISTS skus CASCADE;
 
--- CREATE TABLE products(
---   id integer PRIMARY KEY NOT NULL UNIQUE,
---   name varchar(30) NOT NULL,
---   slogan varchar(110) NOT NULL,
---   description varchar(470) NOT NULL,
---   category varchar(20) NOT NULL,
---   default_price varchar(25) NOT NULL
--- );
+CREATE TABLE products(
+  id integer PRIMARY KEY NOT NULL UNIQUE,
+  name varchar(30) NOT NULL,
+  slogan varchar(110) NOT NULL,
+  description varchar(470) NOT NULL,
+  category varchar(20) NOT NULL,
+  default_price varchar(25) NOT NULL
+);
 
--- CREATE TABLE related(
---   id integer PRIMARY KEY NOT NULL UNIQUE,
---   product_id integer NOT NULL,
---   related_product_id integer NOT NULL REFERENCES products(id)
--- );
+CREATE TABLE related(
+  id integer PRIMARY KEY NOT NULL UNIQUE,
+  product_id integer NOT NULL REFERENCES products(id),
+  related_product_id integer NOT NULL
+);
 
--- CREATE TABLE features(
---   id integer PRIMARY KEY NOT NULL UNIQUE,
---   product_id integer NOT NULL REFERENCES products(id),
---   feature varchar(30) NOT NULL,
---   value varchar(30)
--- );
+CREATE TABLE features(
+  id integer PRIMARY KEY NOT NULL UNIQUE,
+  product_id integer NOT NULL REFERENCES products(id),
+  feature varchar(30) NOT NULL,
+  value varchar(30)
+);
 
--- CREATE TABLE styles(
---   id integer PRIMARY KEY NOT NULL UNIQUE,
---   product_id integer NOT NULL REFERENCES products(id),
---   name varchar(60) NOT NULL,
---   sale_price varchar(10) NULL,
---   original_price varchar(10) NOT NULL,
---   "default?" integer NOT NULL
--- );
+CREATE TABLE styles(
+  id integer PRIMARY KEY NOT NULL UNIQUE,
+  product_id integer NOT NULL REFERENCES products(id),
+  name varchar(60) NOT NULL,
+  sale_price varchar(10) NULL,
+  original_price varchar(10) NOT NULL,
+  "default?" integer NOT NULL
+);
 
 CREATE TABLE photos(
   id integer PRIMARY KEY NOT NULL UNIQUE,
-  style_id integer NOT NULL,-- REFERENCES styles(id),
+  style_id integer NOT NULL REFERENCES styles(id),
   url varchar(150) NOT NULL,
   thumbnail_url varchar(150) NOT NULL
 );
 
--- CREATE TABLE skus(
---   id integer PRIMARY KEY NOT NULL UNIQUE,
---   style_id integer NOT NULL REFERENCES styles(id),
---   size varchar(10) NOT NULL,
---   quantity integer NOT NULL
--- );
+CREATE TABLE skus(
+  id integer PRIMARY KEY NOT NULL UNIQUE,
+  style_id integer NOT NULL REFERENCES styles(id),
+  size varchar(10) NOT NULL,
+  quantity integer NOT NULL
+);
 
--- \COPY products FROM '../database/cleanedSamples/productSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
--- \COPY related FROM '../database/cleanedSamples/relatedSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
--- \COPY features FROM '../database/cleanedSamples/featuresSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
--- \COPY styles FROM '../database/cleanedSamples/stylesSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
-\COPY photos FROM '../database/cleanedSamples/photosSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
--- \COPY skus FROM '../database/cleanedSamples/skusSample.csv' DELIMITER ',' CSV HEADER; -- READY FOR TRANSFORM
+\COPY products FROM '../database/cleanedDatabases/product.csv' DELIMITER ',' CSV HEADER;
+\COPY related FROM '../database/cleanedDatabases/related.csv' DELIMITER ',' CSV HEADER;
+\COPY features FROM '../database/cleanedDatabases/features.csv' DELIMITER ',' CSV HEADER;
+\COPY styles FROM '../database/cleanedDatabases/styles.csv' DELIMITER ',' CSV HEADER;
+\COPY photos FROM '../database/cleanedDatabases/photos-0.csv' DELIMITER ',' CSV HEADER;
+\COPY photos FROM '../database/cleanedDatabases/photos-1.csv' DELIMITER ',' CSV HEADER;
+\COPY skus FROM '../database/cleanedDatabases/skus.csv' DELIMITER ',' CSV HEADER;
